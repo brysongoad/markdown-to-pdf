76d28e23f4a0bd50a05249074e7da05424ed93e4
Bryson Goad <dev@brysongoad.com>
2023-03-21 21:06:22 -0500

    disable cssMinify so the default text in the css tab retains formatting

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/markdown-to-pdf/'
+  base: '/markdown-to-pdf/',
+  build: {
+    cssMinify: false
+  }
 })
